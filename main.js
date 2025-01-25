d450a97c9fae3fac4bcecff316b71827cae31b3e
BiaCarvalhoCavalieri <bia3003carvalho@gmail.com>
2025-01-25 09:34:12 -0300

    adding exercise description

--- main.js
+++ main.js
@@ -1,25 +1,58 @@
 async function executeExercise(exerciseNumber) {
     const resultElement = document.getElementById("result");
     const resultTitleElement = document.getElementById("result-title");
+    const exerciseDescriptionElement = document.getElementById("exercise-description");
     resultTitleElement.innerHTML = `Resultado exercício ${exerciseNumber}:`;
 
     switch (exerciseNumber) {
         case 1:
+            exerciseDescriptionElement.innerHTML = `Enunciado exercício 1: <br>
+                Observe o trecho de código abaixo: <br> 
+                int INDICE = 13, SOMA = 0, K = 0; <br>
+                Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } <br>
+                Imprimir(SOMA); <br>
+                Ao final do processamento, qual será o valor da variável SOMA?`;
             resultElement.innerHTML = `<p>${exercise1()}</p>`;
             break;
         case 2:
+            exerciseDescriptionElement.innerHTML = `Enunciado exercício 2: <br>
+                Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), <br>
+                escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência. <br>
+                IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;`;
             resultElement.innerHTML = `<p>${exercise2()}</p>`;
             break;
         case 3:
+            exerciseDescriptionElement.innerHTML = `Enunciado exercício 3: <br>
+                Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne: <br>
+                • O menor valor de faturamento ocorrido em um dia do mês; <br>
+                • O maior valor de faturamento ocorrido em um dia do mês; <br>
+                • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal. <br>
+                IMPORTANTE: <br>
+                a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal; <br>
+                b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;`;
             resultElement.innerHTML = `<p>${await exercise3()}</p>`;
             break;
         case 4:
+            exerciseDescriptionElement.innerHTML = `Enunciado exercício 4: <br>
+                Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado: <br>
+                • SP – R$67.836,43 <br>
+                • RJ – R$36.678,66 <br>
+                • MG – R$29.229,88 <br>
+                • ES – R$27.165,48 <br>
+                • Outros – R$19.849,53 <br>
+                Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.`;
             resultElement.innerHTML = `<p>${exercise4()}</p>`;
             break;
         case 5:
+            exerciseDescriptionElement.innerHTML = `Enunciado exercício 5: <br>
+                Escreva um programa que inverta os caracteres de uma string. <br>
+                IMPORTANTE: <br>
+                a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código; <br>
+                b) Evite usar funções prontas, como, por exemplo, reverse;`;
             resultElement.innerHTML = `<p>${exercise5()}</p>`;
             break;
         default:
+            resultTitleElement.innerHTML = `<p style="color: red;">Exercício não encontrado!</p>`;
             resultElement.innerHTML = `<p style="color: red;">Exercício não encontrado!</p>`;
             break;
     }
